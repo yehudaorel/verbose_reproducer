10c21caeaf20ba88e55af5d54bec395d629fab3c
Orel Yehuda <orel.yehuda@intel.com>
2023-04-28 06:15:48 +0800

    add formated output (console and csv)

--- reproducer.py
+++ reproducer.py
@@ -103,8 +103,8 @@ def match_logs(a, b):
         a_time = float(item['time'])
         b_time = float(b_dict[key]['time'])
             
-        delta = (((a_time-b_time)/a_time) * 100)
-        diff = (a_time-b_time)
+        delta = round((((a_time-b_time)/a_time) * 100), 4)
+        diff = round((a_time-b_time), 4)
             
             
         curr_op = {'primitive':item['primitive'],
@@ -130,32 +130,19 @@ def output_benchdnn_inputs(lines, prim):
       
      
 
-
-def generate_benchdnn_inputs(prim_kind, benchdnn_input, p_ops):
-  
-  if(prim_kind == "all"):
-    p_ops_prims = [str(elem['primitive']) for i,elem in enumerate(p_ops)]
-    prim_intersection = list(set(benchdnn_input[1].keys()) & set(p_ops_prims))
-    prim_keys = benchdnn_input[1].keys()
-  else:
-    prim_keys = [prim_kind]
-  
-
+def generate_benchdnn_inputs(prim_keys, benchdnn_input, p_ops):
   for key in prim_keys:
-    
-    lines = benchdnn_input[1][key]
-    plines = lines.split("\n")
+    lines = benchdnn_input[1][key].split("\n")
     input_lines = []
     
-    for line in plines:
+    for line in lines:
       temp_line = line.split(" ")
+
       for y, i in enumerate(p_ops):
         if str(i['operation']) == temp_line[len(temp_line) -1]:
           fixed_time = "--fix-times-per-prb=" + str(i['ncalls'])
           temp_line.insert(2, fixed_time)
-          curr_line = " ".join(str(x) for x in temp_line)
-          curr_line = curr_line + "\n"
-          
+          curr_line = " ".join(str(x) for x in temp_line) + "\n"
           input_lines.append(curr_line)
     
           del p_ops[y]
@@ -166,15 +153,21 @@ def generate_benchdnn_inputs(prim_kind, benchdnn_input, p_ops):
   
   
 
-def print_shape_analysis(sorted_ops, is_output):
-  header = ["Primitive" , "Shape" , "NCalls" , "Log1 time(ms)" , "Log2 time(ms)" , "Delta %" , "Difference"]
+def print_shape_analysis(sorted_ops):
+  header = ["Primitive" , "Shape" , "NCalls" , "Log1 time(ms)" , "Log2 time(ms)" , "Delta" , "Difference"]
   
   with open('shape_analysis.csv', 'w', encoding='UTF8') as csv_file:
+    dash = '-' * 136
     writer = csv.writer(csv_file)
     writer.writerow(header)
+    print(dash)
+    print('{:<10s}{:^60s}{:^10s}{:^15s}{:^15s}{:^14s}{:^14s}'.format(header[0],header[1],header[2],header[3],header[4],header[5],header[6]))
+    print(dash)
     
-    
-
+    for i in sorted_ops:
+      print('{:<10s}{:<60}{:^10s}{:^15s}{:^15s}{:^14s}{:^14s}'.format(str(i['primitive']), i['operation'],str(i['ncalls']),str(i['log1_time']) ,str(i['log2_time']) ,str(i['delta']) + "%",str(i['diff'])))
+      row = i.values()
+      writer.writerow(row)
 
 
 
@@ -202,36 +195,40 @@ def main():
     
     log_breakdown1 = parse_log(inputs.log1)
     log_breakdown2 = parse_log(inputs.log2)
-    
-    print(convert_driver("conv"))
-    
 
     a = prepare_list(log_breakdown1, inputs.primitive_kind);
     b = prepare_list(log_breakdown2, inputs.primitive_kind)
     
     sorted_ops = match_logs(a, b)
-    
     ops = []
     
-    
-    counter = 0
-    for i in sorted_ops:
+    for counter, i in enumerate(sorted_ops):
         if((i['delta'] < (1 *(float(inputs.threshold))))):
-            counter = counter +1
             ops.append(i)
-            print(str(i['primitive']) + " | " + i['operation'] + " | " + str(i['log1_time']) + " | " + str(i['log2_time']) + " | " + str(i['delta']) + " | " + str(i['diff']))
       
-      
+    
+    if(len(ops) > 0):
+      print_shape_analysis(ops)
+    
     print("Total matches: " + str(len(sorted_ops)) + " out of " + str(len(a)))
     
-    if(inputs.generate):
+    
+    
+    if(inputs.generate): 
       benchdnn_input = generate_benchdnn_input(inputs.log2)
       prim = convert_driver(inputs.primitive_kind)
+      
       if(prim != None):
-        generate_benchdnn_inputs(prim, benchdnn_input, ops)
+        if(prim == "all"):
+          ops_prims = [str(elem['primitive']) for elem in ops]
+          prim_keys = list(set(benchdnn_input[1].keys()) & set(ops_prims))
+      
+        else:
+          prim_keys = [prim]
+        
+        generate_benchdnn_inputs(prim_keys, benchdnn_input, ops)
         
       
-
 
 
 if __name__ == '__main__':
